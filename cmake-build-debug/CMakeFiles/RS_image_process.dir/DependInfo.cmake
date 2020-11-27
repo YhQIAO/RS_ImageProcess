e6fee6b7b80b69ced86d196a54b86fd78c988fe1
YhQIAO <970445893@qq.com>
2020-11-27 17:46:54 +0800

    codes

---
+++ cmake-build-debug/CMakeFiles/RS_image_process.dir/DependInfo.cmake
@@ -0,0 +1,21 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/Users/qiaoyihong/Library/Mobile Documents/com~apple~CloudDocs/MyUniversity/遥感数字图像处理/RS_ImageProcess/Problem6-2/Problem6-2.cpp" "/Users/qiaoyihong/Library/Mobile Documents/com~apple~CloudDocs/MyUniversity/遥感数字图像处理/RS_ImageProcess/cmake-build-debug/CMakeFiles/RS_image_process.dir/Problem6-2/Problem6-2.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "AppleClang")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/usr/local/Cellar/opencv/4.5.0_5/include/opencv4"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/RS_image_process.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/RS_image_process.dir/Problem6-2/Problem6-2.cpp.o"
+  "RS_image_process"
+  "RS_image_process.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/RS_image_process.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
