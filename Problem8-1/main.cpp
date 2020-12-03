7d00b64768b0386619a77f7c22aba80b248fe410
YhQIAO <49469609+YhQIAO@users.noreply.github.com>
2020-12-03 18:19:34 +0800

    Update main.cpp

--- Problem8-1/main.cpp
+++ Problem8-1/main.cpp
@@ -5,20 +5,35 @@
 #include "MyImageProcessLib.h"
 #include "Brovey.h"
 #include "PCA_Fusion.h"
+#include "HSI_Fusion.h"
 
 using namespace std;
 
 int main() {
-    string multiBandPath = "../../Chap8/data/Landsat8_OLI_multi.dat";
-    // 演示Brovey图像融合
-	// Brovey();
+	string multiBandPath = "../../Chap8/data/Landsat8_OLI_multi.dat";
+	string highImagePath = "../../Chap8/data/Landsat8_OLI_pan.dat";
+
+
+	// 演示Brovey图像融合
+	//Brovey();
 
 	// 演示PCA
 	vector<cv::Mat> multiBandImage = readMultiSpectralImage(
-	        multiBandPath,397, 395, 7, 2,"BIL");
-    string highImagePath = "../../Chap8/data/Landsat8_OLI_pan.dat";
-    cv::Mat highImage = readBinaryImageOfBandX(
-            highImagePath, 796, 792, 1, 1, 12, "BSQ");
-	PCA_Fusion(multiBandImage, highImage);
+		multiBandPath, 397, 395, 7, 2, "BIL");
+	cv::Mat highImage = readBinaryImageOfBandX(
+		highImagePath, 796, 792, 1, 1, 12, "BSQ");
+	// PCA_Fusion(multiBandImage, highImage);
 
+	// 演示HSI融合
+	cv::Mat mul[] = { multiBandImage.at(1),
+		multiBandImage.at(2),multiBandImage.at(3) };
+	cv::Mat multiBGRImage(multiBandImage.at(1).size(), CV_8UC3);
+	cv::merge(mul, 3, multiBGRImage);
+	
+	HSI_Fusion(multiBGRImage, highImage);
+	cv::imshow("multiBGRImage", multiBGRImage);
+	cv::Mat HSIImage = BGR2HSI(multiBGRImage);
+	cv::Mat BGRImage = HSI2BGR(HSIImage);
+	
+	cv::waitKey(0);
 }
